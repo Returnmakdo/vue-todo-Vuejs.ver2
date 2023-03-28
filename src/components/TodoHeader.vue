41827e5311e3d2d5a4989da32743cf350309601f
Returnmakdo <cldud970@naver.com>
2023-03-28 10:33:19 +0900

    vue-todo

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  font-family: 'Ubuntu', sans-serif;
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      v-model="newTodoItem"
+      @keyup.enter="addTodo" />
+    <span
+      class="addContainer"
+      v-on:click="addTodo">
+      <i class="fa-solid fa-plus addBtn"></i>
+    </span>
+    <AlertModal
+      :show="showModal"
+      @close="showModal = false">
+      <template v-slot:header>
+        아무것도 안넣으면 나오는 모달
+        <i
+          class="fas fa-times closeModalBtn"
+          @click="showModal = false"></i>
+      </template>
+    </AlertModal>
+  </div>
+</template>
+
+<script>
+import AlertModal from './common/AlertModal.vue';
+export default {
+  data() {
+    return {
+      newTodoItem: '',
+      showModal: false,
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== '') {
+        this.$emit('addTodoItem', this.newTodoItem);
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    },
+  },
+  components: {
+    AlertModal: AlertModal,
+  },
+};
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
+}
+</style>
